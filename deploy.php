864041f1049b01103324f1305f18a85188388312
Satu Gaya <satu.gaya@edu.sasky.fi>
2021-12-13 09:01:43 +0200

    pieni korjaus

--- deploy.php
+++ deploy.php
@@ -10,7 +10,7 @@ set('application', 'perhekalenteri');
 set('repository', 'git@github.com:satugaya/perhekalenteri.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+//set('git_tty', true); 
 
 // Shared files/dirs between deploys 
 set('shared_files', []);
